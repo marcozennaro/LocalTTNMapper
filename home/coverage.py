b5f38b566cf8e0c8eeba2d54e6838e7ed9cb5377
Marco Zennaro <marcozennaro@gmail.com>
2019-02-26 10:46:17 +0000

    Fixed indenting

--- home/coverage.py
+++ home/coverage.py
@@ -27,16 +27,19 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
 
     themsg = json.loads(str(msg.payload))
-    #print(themsg)
+    print(themsg)
 
     #print(len(themsg['metadata']['gateways']))
 
     payload_raw = themsg["payload_raw"]
     payload_plain = base64.b64decode(payload_raw)
     device = themsg["dev_id"]
-    #print(device) 
-    lat = themsg["payload_fields"]["lat"]
-    lon = themsg["payload_fields"]["lon"]
+    print(device) 
+    lat = themsg["payload_fields"]["latitude"]
+    lon = themsg["payload_fields"]["longitude"]
+
+    print(lat)
+    print(lon)
 
     freq = themsg["metadata"]["frequency"]
     rssi = themsg['metadata']['gateways'][0]['rssi']
@@ -56,67 +59,68 @@ def on_message(client, userdata, msg):
 
     print(clr) 
     
-   file = open("/var/www/html/scoverage/js/nodes.js","a")
-   file.write('var circle = L.circle([')
-   file.write("%f," % lat)
-   file.write("%f" % lon)
-   file.write('], {\n')
-   file.write('color: \'%s\',' % clr)
-   file.write('fillColor: \'%s\',' % clr)
-   file.write('fillOpacity: 0.2,')
-   file.write('radius: 20')
-   file.write('}).addTo(mymap).bindPopup("Device: <b> %s </b>' % device)
-   file.write('<br>Freq: %s' % themsg["metadata"]["frequency"])
-   file.write('<br>DR: %s' % themsg['metadata']['data_rate'])
-   file.write('<br><b>Gateway 1</b>: %s' %
-   themsg['metadata']['gateways'][0]['gtw_id'])
-   file.write('<br>RSSI: %s' % themsg['metadata']['gateways'][0]['rssi'])
-   file.write('<br>SNR: %s' % themsg['metadata']['gateways'][0]['snr'])
+    file = open("/var/www/html/coverage/js/nodes.js","a")
+    file.write('var circle = L.circle([')
+    file.write("%f," % lat)
+    file.write("%f" % lon)
+    file.write('], {\n')
+    file.write('color: \'%s\',' % clr)
+    file.write('fillColor: \'%s\',' % clr)
+    file.write('fillOpacity: 0.2,')
+    file.write('radius: 20')
+    file.write('}).addTo(mymap).bindPopup("Device: <b> %s </b>' % device)
+    file.write('<br>Freq: %s' % themsg["metadata"]["frequency"])
+    file.write('<br>DR: %s' % themsg['metadata']['data_rate'])
+    file.write('<br><b>Gateway 1</b>: %s' %
+    themsg['metadata']['gateways'][0]['gtw_id'])
+    file.write('<br>RSSI: %s' % themsg['metadata']['gateways'][0]['rssi'])
+    file.write('<br>SNR: %s' % themsg['metadata']['gateways'][0]['snr'])
 
-   if len(themsg['metadata']['gateways']) == 1:
-     file.write('");')
-   elif len(themsg['metadata']['gateways']) > 1:
-     file.write('<br><b>Gateway 2</b>: %s' %
-   themsg['metadata']['gateways'][1]['gtw_id'])
-     file.write('<br>RSSI: %s' % themsg['metadata']['gateways'][1]['rssi'])
-     file.write('<br>SNR: %s' % themsg['metadata']['gateways'][1]['snr'])
-     file.write('");')
-   elif len(themsg['metadata']['gateways']) > 2:
-     file.write('<br><b>Gateway 2</b>: %s' %
-   themsg['metadata']['gateways'][1]['gtw_id'])
-     file.write('<br>RSSI: %s' % themsg['metadata']['gateways'][1]['rssi'])
-     file.write('<br>SNR: %s' % themsg['metadata']['gateways'][1]['snr'])
-     file.write('<br><b>Gateway 3</b>: %s' %
-   themsg['metadata']['gateways'][2]['gtw_id'])
-     file.write('<br>RSSI: %s' % themsg['metadata']['gateways'][2]['rssi'])
-     file.write('<br>SNR: %s' % themsg['metadata']['gateways'][2]['snr'])
-     file.write('");')
+    if len(themsg['metadata']['gateways']) == 1:
+      file.write('");')
+    elif len(themsg['metadata']['gateways']) > 1:
+      file.write('<br><b>Gateway 2</b>: %s' %
+      themsg['metadata']['gateways'][1]['gtw_id'])
+      file.write('<br>RSSI: %s' % themsg['metadata']['gateways'][1]['rssi'])
+      file.write('<br>SNR: %s' % themsg['metadata']['gateways'][1]['snr'])
+      file.write('");')
+    elif len(themsg['metadata']['gateways']) > 2:
+      file.write('<br><b>Gateway 2</b>: %s' %
+      themsg['metadata']['gateways'][1]['gtw_id'])
+      file.write('<br>RSSI: %s' % themsg['metadata']['gateways'][1]['rssi'])
+      file.write('<br>SNR: %s' % themsg['metadata']['gateways'][1]['snr'])
+      file.write('<br><b>Gateway 3</b>: %s' %
+      themsg['metadata']['gateways'][2]['gtw_id'])
+      file.write('<br>RSSI: %s' % themsg['metadata']['gateways'][2]['rssi'])
+      file.write('<br>SNR: %s' % themsg['metadata']['gateways'][2]['snr'])
+      file.write('");')
    #draw lines
-   linefromlat=themsg["payload_fields"]["latitude"]
-   linetolat=themsg['metadata']['gateways'][0]['latitude']
-   linefromlon=themsg["payload_fields"]["longitude"]
-   linetolon=themsg['metadata']['gateways'][0]['longitude']
+   
+    linefromlat=themsg["payload_fields"]["latitude"]
+    linetolat=themsg['metadata']['gateways'][0]['latitude']
+    linefromlon=themsg["payload_fields"]["longitude"]
+    linetolon=themsg['metadata']['gateways'][0]['longitude']
 
-   file.write('\nvar latlngs = [[')
-   file.write("%f" % linefromlat)
-   file.write(',')
-   file.write("%f" % linefromlon)
-   file.write('],[')
-   file.write("%f" % linetolat)
-   file.write(',')
-   file.write("%f" % linetolon)
-   file.write(']];\n')
-   file.write('var polyline = L.polyline(latlngs, {color: \'')
-   file.write(clr)
-   file.write('\'}).addTo(mymap);')
+    file.write('\nvar latlngs = [[')
+    file.write("%f" % linefromlat)
+    file.write(',')
+    file.write("%f" % linefromlon)
+    file.write('],[')
+    file.write("%f" % linetolat)
+    file.write(',')
+    file.write("%f" % linetolon)
+    file.write(']];\n')
+    file.write('var polyline = L.polyline(latlngs, {color: \'')
+    file.write(clr)
+    file.write('\'}).addTo(mymap);')
    #EO draw lines
 
-   file.write("\n")
-   file.close()
+    file.write("\n")
+    file.close()
         
  
 client = mqtt.Client()
-client.username_pw_set("coverage_mapping_XXX", password="ttn-account-v2.KQrXXBVBVX2pia39eFBUNpLMzXNNH1gZxFXsr2eNxY")
+client.username_pw_set("coverage_mapping_XXX", password="ttn-account-v2.KQrBOatXXXX2pia39eFBUNpLMzXNNH1gZxFXsr2eNxY")
 
 client.on_connect = on_connect
 client.on_message = on_message
@@ -128,5 +132,8 @@ client.connect(THE_BROKER, 1883, 60)
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
 client.loop_forever()
+
+
+
 
 
